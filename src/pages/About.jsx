31d19b9f90f76a14a216a2c6c36a1c99aa85850d
Cristo RPS <cristobits0101@gmail.com>
2024-08-19 17:44:02 +0100

    Changing text and imports

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
-import personalImage from "../assets/images/personal.webp";
 import CV from "../components/cv/CV";
 import Networks from "../components/networks/Networks";
+import personalImage from "../assets/images/personal.webp";
 
 const About = () => {
   return (
@@ -9,9 +9,9 @@ const About = () => {
         <img src={personalImage} alt="Cristo Suárez" />
       </article>
       <article className="personal-information-container">
-        <h2>Greetings and welcome to my portfolio!</h2>
-        <h1>I am Cristo Suárez</h1>
-        <p>💻 React & Spring Developer</p>
+        <h2>👋 Greetings and welcome to my portfolio!</h2>
+        <h1>Cristo Suárez</h1>
+        <p>💻 Skilled React and Spring Developer</p>
         <p>✉️ cristobits0101@gmail.com</p>
         <CV />
         <Networks />

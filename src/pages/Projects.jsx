884986774cecf9e0a2d0832236ebd6c78747a369
Cristo RPS <cristobits0101@gmail.com>
2024-08-20 17:18:54 +0100

    Updating content

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,7 @@
 const Projects = () => {
   return (
     <article className="projects-main-article">
-      <h2 className="projects-main-article-h2">Projects</h2>
+      <h2 className="projects-main-article-h2">Arrímate</h2>
       <section className="projects-main-article-section-one"></section>
       <section className="projects-main-article-section-two"></section>
     </article>

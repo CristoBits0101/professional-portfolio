6421cbaab99664cbaddae190e03b6a4ab78755f4
Cristo RPS <cristobits0101@gmail.com>
2024-08-20 15:33:05 +0100

    Updating structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-
 import Header from "./layouts/Header";
+import Main from "./layouts/Main";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+      <Main />
+    </>
   );
 }
 
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "../pages/About";
+import Projects from "../pages/Projects";
+
+export default function Main() {
+    return (
+        <main>
+            <Routes>
+                <Route path="/" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
+        </main>
+    )
+}

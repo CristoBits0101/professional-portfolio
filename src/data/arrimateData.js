3433ff5f748c288d758cb32e625be396cef11bff
Cristo RPS <cristobits0101@gmail.com>
2025-01-05 17:53:32 +0000

    Fix typo in technologies list and simplify the array of technologies in arrimateData

--- src/data/arrimateData.js
+++ src/data/arrimateData.js
@@ -1,8 +1,8 @@
 const arrimate = {
   name: 'Arrímate',
   description: '🔹Social Network with E-commerce and Artificial Intelligence',
-  technologies:
-    '🔹HTML • Tailwind CSS • Shadcn UI • TypeScript • Node.js • Next.js • Prisma Client • PostgreSQL • Next-Intl • Auth.js • OAuth 2.0 • Bcrypt • Bcrypt.js • React Hook Form • Zod • UUID • Resend • Git • GitHub • Vercel • Neon • Cloudinary • Google Cloud • Google Maps • React Icons • React Spinners • Swiper • Scrum • Trunk Based Development • Screaming Architecture',
+  tecnologies:
+    '🔹HTML • Tailwind CSS • Shadcn • TypeScript • Node.js • Next.js • PostgreSQL',
   state: '🛠️ En progreso',
   video: 'https://www.youtube.com/watch?v=vBvmeIDnkuY',
   links: {

368ad74a40cd822fe8dfa3ae31c69b77c0ebd90e
Cristo RPS <cristobits0101@gmail.com>
2025-01-05 15:38:10 +0000

    Update cloud technologies in arrimateData for comprehensive information

--- src/data/arrimateData.js
+++ src/data/arrimateData.js
@@ -2,7 +2,7 @@ const arrimate = {
   name: 'Arrímate',
   description: '🔹Social Network with E-commerce and Artificial Intelligence',
   tecnologies: '🔹HTML • Tailwind CSS • TypeScript • Node.js • Next.js • PostgreSQL',
-  cloud : '🔹Vercel • Neon Serverless Postgres • Cloudinary',
+  cloud : '🔹Vercel • Neon Serverless Postgres • Cloudinary • Git • GitHub',
   google: '🔹Google Cloud • Google Maps • Google OAuth 2.0',
   state: '🛠️ En progreso',
   video: 'https://www.youtube.com/watch?v=vBvmeIDnkuY',
